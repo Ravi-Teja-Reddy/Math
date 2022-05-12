a4696df7efa627d2dd01dbd4424ddc609b74bfca
Ravi-Teja-Reddy <59519723+Ravi-Teja-Reddy@users.noreply.github.com>
2022-05-12 11:25:41 +0530

    Time: 4 ms (27.82%), Space: 5.9 MB (45.53%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,29 +1,30 @@
-class Solution {
-public:
-    double myPow(double x, int n) {
-        
-        if(n==1)
-            return x;
-        if(n==-1)
-            return 1/x;
-        
-        if(n==0)
-            return 1.0;
-        
-        if(n%2==0)
+class Solution
+{
+    public:
+        double myPow(double x, int n)
         {
-            double smalloutput=myPow(x,n/2);
-           
-            return smalloutput*smalloutput;
-        }
-        
-        else
-        {
-             double smalloutput=myPow(x,n/2);
-            if(n>0)
-            return smalloutput*smalloutput*x;
+
+            if (n == 1)
+                return x;
+            if (n == -1)
+                return 1 / x;
+
+            if (n == 0)
+                return 1.0;
+
+            if (n % 2 == 0)
+            {
+                double smalloutput = myPow(x, n / 2);
+
+                return smalloutput * smalloutput;
+            }
             else
-                return smalloutput*smalloutput*1/x;
+            {
+                double smalloutput = myPow(x, n / 2);
+                if (n > 0)
+                    return smalloutput *smalloutput * x;
+                else
+                    return smalloutput *smalloutput *1 / x;
+            }
         }
-    }
 };
